6ddec151ca16958d3a59c63bc73d0f906a2f3579
trmti <trmtikko@gmail.com>
2022-03-24 23:25:41 +0900

    CORS変更

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8000'
+    origins 'http://localhost:8000', "https://file-sharing-app3.vercel.app"
 
     resource '*',
       headers: :any,
